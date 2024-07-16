55dd1d6dea65878eb6ca0be5ca1b5514b462a193
Khanh Do <khanhdo1219@gmail.com>
2024-07-16 23:51:09 +0700

    setup login & db

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-white rounded-full p-4">
+        <Image src="/logo.svg" alt="Logo" height={60} width={60} />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">Twitch</p>
+        <p className="text-sm text-muted-foreground">Let&apos;s play</p>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
