b18c297d0f360e1a92529f9a910970eda821ea4d
Khanh Do <khanhdo1219@gmail.com>
2024-07-22 17:52:48 +0700

    setup chat

---
+++ app/(dashboard)/u/[username]/(home)/loading.tsx
@@ -0,0 +1,11 @@
+import { StreamPlayerSkeleton } from "@/components/stream-playyer";
+
+const CreatorLoading = () => {
+  return (
+    <div className="h-full">
+      <StreamPlayerSkeleton />
+    </div>
+  );
+};
+
+export default CreatorLoading;
--- components/stream-playyer/video.tsx
+++ components/stream-playyer/video.tsx
@@ -5,6 +5,7 @@ import { ConnectionState, Track } from "livekit-client";
 import { LoadingVideo } from "./loading-video";
 import { OfflineVideo } from "./offline-video";
 import { LiveVideo } from "./live-video";
+import { Skeleton } from "../ui/skeleton";
 
 interface VideoProps {
   hostName: string;
@@ -29,4 +30,12 @@ export const Video = ({ hostName, hostIdentity }: VideoProps) => {
   }
 
   return <div className="aspect-video border-b group relative">{content}</div>;
+};
+
+export const VideoSkeleton = () => {
+  return (
+    <div className="aspect-video border-x border-background">
+      <Skeleton className="h-full w-full rounded-none" />
+    </div>
+  );
 };
