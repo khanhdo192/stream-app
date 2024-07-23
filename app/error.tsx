3277cf891f8691c37d8241ea17cb2540e1a2d6a5
Khanh Do <khanhdo1219@gmail.com>
2024-07-23 20:05:26 +0700

    hot fix

--- app/error.tsx
+++ app/error.tsx
@@ -2,19 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 const ErrorPage = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (pathname === "/") {
-      router.refresh();
-    }
-  }, [pathname, router]);
-
   return (
     <div className="h-[calc(100vh-80px)] flex flex-col space-y-4 items-center justify-center text-muted-foreground">
       <p>Something went wrong</p>
